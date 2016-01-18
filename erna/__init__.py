c3033e2333c4d5bb0aed4c3164d940e7829d351d
Kai Bruegge <kai.bruegge@udo.edu>
2016-01-18 15:24:31 +0100

    fix output format bug. hopefully

--- erna/__init__.py
+++ erna/__init__.py
@@ -26,18 +26,19 @@ def collect_output(job_outputs, output_path):
     logger.info("There are a total of {} events in the result".format(len(df)))
 
     name, extension = os.path.splitext(output_path)
-    if extension == 'json':
+    if extension not in ['json', 'h5', 'hdf5', 'hdf' , 'csv']:
+        logger.warn("Did not recognize file extension {}. Writing to JSON".format(extension))
+        df.to_json(output_path, orient='records', date_format='epoch' )
+    elif extension == 'json':
         logger.info("Writing JSON to {}".format(output_path))
         df.to_json(output_path, orient='records', date_format='epoch' )
-    elif extension == 'h5' or extension == 'hdf' or extension == 'hdf5':
+    elif extension in ['h5', 'hdf','hdf5']:
         logger.info("Writing HDF5 to {}".format(output_path))
         df.to_hdf(output_path, 'table', mode='w')
     elif extension == 'csv':
         logger.info("Writing CSV to {}".format(output_path))
         df.to_csv(output_path)
-    else:
-        logger.warn("Did not recognize file extension {}. Writing to JSON".format(extension))
-        df.to_json(output_path, orient='records', date_format='epoch' )
+
 
 
 def load(earliest_night, latest_night, path_to_data, factdb,source_name="Crab", timedelta_in_minutes="30"):
