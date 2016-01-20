128d29058cc7bc4b0b8f8463b0dcc2d40f1ebb45
Kai Bruegge <kai.bruegge@udo.edu>
2016-01-20 10:15:14 +0100

    speed up aux_reader

--- read_aux.py
+++ read_aux.py
@@ -1,12 +1,11 @@
 from astropy.io import fits
 # from IPython import embed
 import pandas as pd
-import numpy as np
 from sqlalchemy import create_engine
 import glob
 import os
 import click
-from tqdm import *
+from tqdm import tqdm
 
 @click.command()
 @click.argument('aux_folder', type=click.Path(file_okay=False, dir_okay=True, exists=True))
@@ -28,13 +27,22 @@ def main(aux_folder, sqlite_file, chunksize, behaviour):
     # print("Found {} pointing position files in path {}".format(len(pointing_files), aux_folder))
     # df_pointing = create_dataframe(pointing_files)
 
-    tracking_files = glob.glob(aux_folder + "/**/*DRIVE_CONTROL_TRACKING_POSITION.fits", recursive=True)
-    print("Found {} tracking position files in path {}".format(len(tracking_files), aux_folder))
-    df_tracking = create_dataframe(tracking_files)
+    drive_files = glob.glob(aux_folder + "/**/*DRIVE_CONTROL_{TRACKING,SOURCE}_POSITION.fits", recursive=True)
+    print("Found {} tracking position files in path {}".format(len(drive_files), aux_folder))
+    df_source = pd.DataFrame()
+    df_tracking = pd.DataFrame()
 
-    source_files = glob.glob(aux_folder + "/**/*DRIVE_CONTROL_SOURCE_POSITION.fits", recursive=True)
-    print("Found {} source position files in path {}".format(len(source_files), aux_folder))
-    df_source = create_dataframe(source_files)
+    keys_source=['Time', 'Name', 'Ra_src', 'Dec_src', 'Angle', 'Offset', 'Dec_cmd', 'Ra_cmd']
+    keys_tracking=['Time', 'Ra', 'Dec', 'Az', 'Zd', 'Ha', 'dZd', 'dAz', 'dev']
+
+    for f in tqdm(drive_files):
+        if "SOURCE" in f:
+            append_to_dataframe(df_source, f, keys_source)
+        if "TRACKING" in f:
+            append_to_dataframe(df_tracking, f, keys_tracking)
+
+    create_time_index(df_source)
+    create_time_index(df_tracking)
 
     print("Writing {} entries to db. This might take a while".format(len(df_source)+len(df_tracking)))
     # print("Adding pointing data.")
@@ -45,47 +53,45 @@ def main(aux_folder, sqlite_file, chunksize, behaviour):
     df_source.to_sql('DRIVE_CONTROL_SOURCE_POSITION', engine, index=True,  if_exists=behaviour , chunksize=chunksize)
 
     # result = engine.execute('CREATE UNIQUE INDEX time_pointing ON DRIVE_CONTROL_POINTING_POSITION(time_in_seconds)')
-    result = engine.execute('CREATE INDEX time_tracking ON DRIVE_CONTROL_TRACKING_POSITION(time_in_seconds)')
-    result = engine.execute('CREATE INDEX time_source ON DRIVE_CONTROL_SOURCE_POSITION(time_in_seconds)')
+    engine.execute('CREATE INDEX time_tracking ON DRIVE_CONTROL_TRACKING_POSITION(time_in_seconds)')
+    engine.execute('CREATE INDEX time_source ON DRIVE_CONTROL_SOURCE_POSITION(time_in_seconds)')
 
     statinfo = os.stat(sqlite_file)
-    print("DB has {} MegaBytes".format(statinfo.st_size/1000000.0))
+    print("DB has approximatly {} MegaBytes".format(statinfo.st_size/1000000.0))
 
-def create_dataframe(files):
-    df_source = pd.DataFrame()
-    for f in tqdm(files):
-        #catch some common errors in the fits files
-        try:
-            hdulist = fits.open(f)
-            table = hdulist[1].data
+def create_time_index(df):
+    df['Time'] *=  86400
+    df  = df.set_index(pd.to_datetime(df['Time'], unit='s'))
+    df.rename(columns={'Time':'time_in_seconds'}, inplace=True)
 
-            df = pd.DataFrame()
-            for key in table.names:
-                try:
-                    df[key] = table[key]
-                except ValuerError:
-                    print('Swapped byteorder for key: ', key)
-                    df[key] = table[key].byteswap().newbyteorder()
-            df_source = df_source.append(df)
+def append_to_dataframe(df_aux, drive_file, keys):
+    try:
+        hdulist = fits.open(drive_file)
+        table = hdulist[1].data
 
-        except IndexError as e:
-            print("File {} seems corrupted. Hdulist : {}".format(f, hdulist))
-            print("Skipping to next file.")
-            continue
-        except OSError as e:
-            print("File {} seems corrupted. Could not open file. Header corrupt?.  ".format(f))
-            print("Skipping to next file.")
-            continue
-        except Exception as e:
-            print("Errors occured while reading the fits file. ")
-            print("Skipping to next file.")
-            continue
+        df = pd.DataFrame()
+        for key in keys:
+            try:
+                df[key] = table[key]
+            except ValueError:
+                print('Swapped byteorder for key: ', key)
+                df[key] = table[key].byteswap().newbyteorder()
+        df_aux = df_aux.append(df)
 
-    df_source['Time'] *=  86400
-    df_source  = df_source.set_index(pd.to_datetime(df_source['Time'], unit='s'))
-    df_source.rename(columns={'Time':'time_in_seconds'}, inplace=True)
+    except IndexError:
+        print("File {} seems corrupted. Hdulist : {}".format(drive_file, hdulist))
+        print("Skipping to next file.")
+        continue
+    except OSError:
+        print("File {} seems corrupted. Could not open file. Header corrupt?.  ".format(drive_file))
+        print("Skipping to next file.")
+        continue
+    except Exception:
+        print("Errors occured while reading the fits file. ")
+        print("Skipping to next file.")
+        continue
 
-    return df_source
+    return df_aux
 
 
 if __name__ == "__main__":
