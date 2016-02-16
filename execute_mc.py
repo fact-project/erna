99633ba1f308ad4f3907602de9e77202a5fbc140
jebuss <jens.buss@udo.edu>
2016-02-16 12:51:04 +0100

    changed mc pattern to also match non zipped files

--- execute_mc.py
+++ execute_mc.py
@@ -66,7 +66,7 @@ def main( jar, xml, out,drs_file, mc_path, queue, engine, num_jobs, vmem, log_le
     # embed()
     for folder in tqdm(mc_path):
         # print("Entering folder {}".format(folder))
-        pattern = path.join(folder, '**/*_Events.fits.gz')
+        pattern = path.join(folder, '**/*_Events.fit*')
         f = glob.glob(pattern, recursive=True)
         files = files + f
 
