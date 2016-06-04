5e7d2b960a493b479ac8be54510d1c3ab2c6290b
jebuss <jens.buss@udo.edu>
2016-06-04 02:46:45 +0200

    created hdf files directly in the facttools excecuter

--- erna/scripts/facttools_executer.py
+++ erna/scripts/facttools_executer.py
@@ -2,14 +2,15 @@ import subprocess
 import os
 import logging
 import tempfile
-
+import json
+import pandas as pd
 
 def main():
     '''
     This is what will be executed on the cluster
     '''
     logger = logging.getLogger(__name__)
-    logger.info("stream runner has been started.")
+    logger.info("facttools executor has been started.")
 
     input_path  = os.environ.get("INPUTFILE")
     jar         = os.environ.get("JARFILE")
@@ -51,6 +52,30 @@ def main():
             logger.error("fact-tools error")
 
     os.remove(input_path)
+
+    #try to read nans else return empty frame
+    with open(output_path,'r') as text:
+        try:
+            logger.info("Reading fact-tools output.")
+            y=json.loads(text.read())
+            df_out=pd.DataFrame(y)
+            df_out["fact_tools"] = os.path.basename(jar)
+            df_out["xml"] = os.path.basename(xml)
+
+            logger.info("Saving data to hdf with {} entries".format(len(df_out)))
+            name, extension = os.path.splitext(output_path)
+            df_out.to_json(os.path.abspath(name+".hdf"), orient='records', date_format='epoch')
+            os.remove(output_path)
+
+        except ValueError as e:
+            logger.error("Fact-tools output could not be read.")
+            print(e)
+
+        except Exception as e:
+            print(e)
+            logger.error( "error gathering output")
+
+
 
 if __name__ == "__main__":
     main()
