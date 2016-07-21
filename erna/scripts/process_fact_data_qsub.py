c01c3b870b45635217c447a1cbc6a87ee32f6ec7
jebuss <jens.buss@udo.edu>
2016-07-21 19:46:57 +0200

    moved sub command generation to sub package

--- erna/scripts/process_fact_data_qsub.py
+++ erna/scripts/process_fact_data_qsub.py
@@ -45,100 +45,7 @@ def read_outputs_to_list(job_output_paths):
     return job_outputs
 
 
-def generate_qsub_command(name, queue, jar, xml, inputfile, outputfile, dbpath,
-                          mail_address,mail_setting, stdout, stderr, engine, script):
-    command_template = []
-    command_template.append("qsub")
-    command_template.append("-N {name}")
-    command_template.append("-q {queue}")
-    command_template.append("-M {mail_address}")
-    command_template.append("-m {mail_setting}")
-    command_template.append("-v {env}")
-    if engine == "SGE":
-        command_template.append("-b yes")
-    command_template.append("-o {stdout}")
-    command_template.append("-e {stderr}")
-    command_template.append("{script}")
 
-    command_template = " ".join(command_template)
-
-    env = []
-    env.append("INPUTFILE={0}".format(inputfile))
-    env.append("JARFILE={0}".format(jar))
-    env.append("XMLFILE={0}".format(xml))
-    env.append("OUTPUTFILE={0}".format(outputfile))
-    env.append("DBPATH={0}".format(dbpath))
-    env.append("JOBNAME={0}".format(name))
-    env.append("PATH={0}".format(os.environ.get("PATH")))
-    env = ",".join(env)
-
-    settings = {
-        "name": name,
-        "queue": queue,
-        "mail_address": mail_address,
-        "mail_setting": mail_setting,
-        "env": env,
-        "stdout": stdout,
-        "stderr": stderr,
-        "script": script,
-    }
-
-    return command_template.format(**settings)
-
-
-def submit_qsub_jobs(jobname, jar, xml, db_path, df_mapping,  engine, queue,
-                     vmem, num_jobs_per_bunch, walltime, dbpath, mail_setting):
-    jobs = []
-    # create job objects
-    # split_indices = np.array_split(np.arange(len(df_mapping)), num_jobs)
-    df_mapping["bunch_index"]= np.arange(len(df_mapping)) // num_jobs_per_bunch
-
-    tempfolder = os.environ.get('DEFAULT_TEMP_DIR')
-    mail_address = os.environ.get('ERROR_MAIL_RECIPIENT')
-
-    jobname = "_".join(jobname.split())
-
-    for num, group in df_mapping.groupby("bunch_index"):
-        df_jobs=group.copy()
-        job_name = "{}_{}".format(jobname, num)
-
-        stdout_file = os.path.join(tempfolder, job_name+".o")
-        stderr_file = os.path.join(tempfolder, job_name+".e")
-        input_path = os.path.join(tempfolder, job_name+".json")
-        output_path = os.path.join(tempfolder, job_name+"_out")
-
-        group.to_json(input_path, orient='records', date_format='epoch')
-
-        command = generate_qsub_command(name=job_name, queue=queue, jar=jar,
-                                        xml=xml, inputfile=input_path,
-                                        outputfile=output_path, dbpath=dbpath,
-                                        mail_address=mail_address,
-                                        mail_setting=mail_setting,
-                                        stdout=stdout_file, stderr=stderr_file,
-                                        engine=engine,
-                                        script="facttools_executer")
-
-        return_code = subprocess.check_output(command, shell=True)
-        logger.info(return_code.decode())
-
-        if engine == "SGE":
-            df_jobs["JOBID"] = int(str(return_code.decode()).split(" ")[2])
-        if engine == "PBS":
-            df_jobs["JOBID"] = int(str(return_code.decode()).split(".")[0])
-
-
-        df_jobs["output_path"] = output_path
-        df_jobs["bunch_index"] = num
-        df_jobs["command"] = command
-        df_jobs["fact_tools"] = os.path.basename(jar)
-        df_jobs["xml"] = os.path.basename(xml)
-        jobs.append(df_jobs)
-            # if num>=1:
-            #     break
-    if len(jobs) == 0:
-        return pd.DataFrame()
-
-    return pd.concat(jobs)
 
 
 @click.command()
@@ -221,7 +128,7 @@ def main(earliest_night, latest_night, data_dir, jar, xml, db, out, queue, mail,
         if ( n_toqueue > 0 ) and ( len(df_runs) > 0):
             df_to_submit = df_runs.head(n_toqueue*num_runs).copy()
             processing_identifier = "{}_{}".format(source, time.strftime('%Y%m%d%H%M'))
-            df_submitted_last = submit_qsub_jobs(processing_identifier, jarpath, xmlpath, db_path, df_to_submit,  engine, queue, vmem, num_runs, walltime, db, mail)
+            df_submitted_last = q.submit_qsub_jobs(processing_identifier, jarpath, xmlpath, db_path, df_to_submit,  engine, queue, vmem, num_runs, walltime, db, mail)
             df_submitted = df_submitted.append(df_submitted_last)
 
 
