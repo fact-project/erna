e62d003eeb39587ab14882209642d5b56cc977bc
jebuss <jens.buss@udo.edu>
2016-09-19 11:11:49 +0200

    demanding fact_tools version to only process data with a tagged fact-tools version

--- erna/database/models.py
+++ erna/database/models.py
@@ -14,7 +14,7 @@ class FactToolsProcessing(BaseModel):
     night = IntegerField()
     run_id = IntegerField()
     used_drs_file = TextField(null=True)
-    fact_tools_jar = TextField(null=True)
+    fact_tools_version = TextField(null=True)
     output_path = TextField(null=True)
     md5hash = CharField(null=True)
     status = CharField(null=True)
