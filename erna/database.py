491b6910eef4f47d78a1c16ad9703f1988f8f1e8
jebuss <jens.buss@udo.edu>
2016-09-27 18:43:14 +0200

    changed re matching to also fit long paths and unzipped fits files

--- erna/database.py
+++ erna/database.py
@@ -12,8 +12,8 @@ log = logging.getLogger(__name__)
 __all__ = ['RawDataFile', 'DrsFile', 'FactToolsRun']
 
 
-datafile_re = re.compile(r'([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{3}).fits.[fg]z')
-drsfile_re = re.compile(r'([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{3}).drs.fits.gz')
+datafile_re = re.compile(r'(?:.*/)?([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{3})\.fits(?:\.[fg]z)?$')
+drsfile_re = re.compile(r'(?:.*/)?([0-9]{4})([0-9]{2})([0-9]{2})_([0-9]{3})\.drs\.fits(?:\.gz)?$')
 
 
 def parse_path(path):
