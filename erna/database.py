71e1292875206f6bc1fe5e76d339f93c27e63c7a
jens buß <jens.buss@tu-dortmund.de>
2016-10-31 10:45:37 +0100

    use upsert for putting entries into data bases to make shure that existing entries are updated

--- erna/database.py
+++ erna/database.py
@@ -118,11 +118,11 @@ def fill_data_runs(df, database):
     df.rename(columns={'fNight': 'night', 'fRunID': 'run_id'}, inplace=True)
     df.drop(['fDrsStep', 'fRunTypeKey'], axis=1, inplace=True)
     with database.atomic():
-        RawDataFile.insert_many(df.to_dict(orient='records')).execute()
+        RawDataFile.insert_many(df.to_dict(orient='records')).upsert().execute()
 
 
 def fill_drs_runs(df, database):
     df.rename(columns={'fNight': 'night', 'fRunID': 'run_id'}, inplace=True)
     df.drop(['fDrsStep', 'fRunTypeKey'], axis=1, inplace=True)
     with database.atomic():
-        DrsFile.insert_many(df.to_dict(orient='records')).execute()
+        DrsFile.insert_many(df.to_dict(orient='records')).upsert().execute()
