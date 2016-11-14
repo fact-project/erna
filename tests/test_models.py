5041965a232842a5dc1b38dee535c2f88e619362
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-11-14 13:03:22 +0100

    Add test to see if init_database can be called on existing db

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,3 +13,18 @@ def test_init():
         )
         with test_database(test_db, models.MODELS):
             models.init_database(test_db)
+
+
+def test_init_twice():
+    from peewee import SqliteDatabase
+    from erna.automatic_processing import models
+    import tempfile
+
+    with tempfile.NamedTemporaryFile() as f:
+        test_db = SqliteDatabase(
+            f.name,
+            fields={'night': 'INTEGER', 'longblob': 'BLOB'}
+        )
+        with test_database(test_db, models.MODELS):
+            models.init_database(test_db)
+            models.init_database(test_db)
