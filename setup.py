7130fda3b65abfb4631050780bcbbaa76322b971
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-11-14 12:40:34 +0100

    Add setup.cfg and modify setup.py for py.test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.2',
     description='Easy RuN Access. Tools that help to do batch processing of FACT data',
     url='https://github.com/fact-project/erna',
-    author='Kai Brügge',
+    author='Kai Brügge, Jens Buss, Maximilian Nöthe',
     author_email='kai.bruegge@tu-dortmund.de',
     license='BEER',
     packages=[
@@ -13,7 +13,6 @@ setup(
         'erna.scripts',
         'erna.automatic_processing',
     ],
-    # dependency_links = ['git+https://github.com/mackaiver/gridmap.git#egg=gridmap'],
     package_data={
         'erna': ['resources/*'],
     },
@@ -26,29 +25,30 @@ setup(
         'PyMySQL',          # in anaconda
         'pytz',             # in anaconda
         'tables',           # needs to be installed by pip for some reason
-        # 'hdf5',
         'click',
         'drmaa',
         'pyzmq',
         'peewee',
         'numexpr',
         'pyyaml',
-        'pytest', # also in  conda
+        'pytest',           # also in  conda
         # 'gridmap>=0.13.1', install from https://github.com/mackaiver/gridmap'
     ],
-   zip_safe=False,
-   entry_points={
-    'console_scripts': [
-        'process_fact_data = erna.scripts.process_fact_data:main',
-        'process_fact_data_qsub = erna.scripts.process_fact_data_qsub:main',
-        'process_fact_mc = erna.scripts.process_fact_mc:main',
-        'fetch_fact_runs = erna.scripts.fetch_fact_runs:main',
-        'process_fact_run_list = erna.scripts.process_fact_run_list:main',
-        'read_aux_files_to_sqlite = erna.scripts.read_aux_files_to_sqlite:main',
-        'facttools_executer = erna.scripts.facttools_executer:main',
-        'erna_fill_database = erna.scripts.fill_database:main',
-        'erna_check_availability = erna.scripts.check_availability:main',
-        'erna_create_tables = erna.scripts.create_db:main',
-    ],
-  }
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'process_fact_data = erna.scripts.process_fact_data:main',
+            'process_fact_data_qsub = erna.scripts.process_fact_data_qsub:main',
+            'process_fact_mc = erna.scripts.process_fact_mc:main',
+            'fetch_fact_runs = erna.scripts.fetch_fact_runs:main',
+            'process_fact_run_list = erna.scripts.process_fact_run_list:main',
+            'read_aux_files_to_sqlite = erna.scripts.read_aux_files_to_sqlite:main',
+            'facttools_executer = erna.scripts.facttools_executer:main',
+            'erna_fill_database = erna.scripts.fill_database:main',
+            'erna_check_availability = erna.scripts.check_availability:main',
+            'erna_create_tables = erna.scripts.create_db:main',
+        ],
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest>=3.0.0'],
 )
