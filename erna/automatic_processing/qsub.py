cd55a2895db650c8a1273fb565eaeaedf0084ff2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-11-28 14:31:43 +0100

    Add function to build qsub command on isdc

--- erna/automatic_processing/qsub.py
+++ erna/automatic_processing/qsub.py
@@ -4,7 +4,8 @@ import os
 import pandas as pd
 
 
-def parse_qstat_xml(user=None):
+def get_current_jobs(user=None):
+    ''' Return a dataframe with current jobs of user '''
     user = user or os.environ['USER']
     xml = sp.check_output(['qstat', '-u', user, '-xml']).decode()
     data = xmltodict.parse(xml)
@@ -23,3 +24,52 @@ def parse_qstat_xml(user=None):
     df = df.astype({'job_number': int, 'priority': float})
     df['start_time'] = pd.to_datetime(df['start_time'])
     return df
+
+
+def build_qsub_command(
+        executable,
+        stdout=None,
+        stderr=None,
+        job_name=None,
+        queue='fact_short',
+        mail_address=None,
+        mail_settings='a',
+        environment=None,
+        resources=None,
+        ):
+    command = []
+    command.append('qsub')
+
+    if job_name:
+        command.extend(['-N', job_name])
+
+    command.extend(['-q', queue])
+    if mail_address:
+        command.extend(['-M', mail_address])
+
+    command.extend(['-m', mail_settings])
+    command.extend(['-b', 'yes'])  # allow a binary executable
+
+    if stdout:
+        command.extend(['-o', stdout])
+
+    if stderr:
+        command.extend(['-e', stderr])
+
+    if environment:
+        command.append('-v')
+        command.append(','.join(
+            '{}={}'.format(k, v)
+            for k, v in environment.items()
+        ))
+
+    if resources:
+        command.append('-l')
+        command.append(','.join(
+            '{}={}'.format(k, v)
+            for k, v in resources.items()
+        ))
+
+    command.append(executable)
+
+    return command
