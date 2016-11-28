f204c892c338b4840d5f148e0cd0c4dd45fb79e1
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-11-28 13:31:22 +0100

    Rename columns and correct dtypes

--- erna/qsub.py
+++ erna/qsub.py
@@ -36,7 +36,21 @@ def parse_qstat_xml(user=None):
     user = user or os.environ['USER']
     xml = subprocess.check_output(['qstat', '-u', user, '-xml']).decode()
     data = xmltodict.parse(xml)
-    return pd.DataFrame.from_dict(data['job_info']['queue_info']['job_list'])
+    df = pd.DataFrame.from_dict(data['job_info']['queue_info']['job_list'])
+
+    df.drop('state', axis=1, inplace=True)
+    df.rename(inplace=True, columns={
+        '@state': 'state',
+        'JB_owner': 'owner',
+        'JB_name': 'name',
+        'JB_job_number': 'job_number',
+        'JAT_prio': 'priority',
+        'JAT_start_time': 'start_time',
+    })
+
+    df = df.astype({'job_number': int, 'priority': float})
+    df['start_time'] = pd.to_datetime(df['start_time'])
+    return df
 
 
 def get_finished_jobs(job_ids):
