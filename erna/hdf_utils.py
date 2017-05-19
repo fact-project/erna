dd9de00355f09e63ac36e60af92fb6a06e32f959
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-05-19 13:13:40 +0200

    Rename columns to snake case and add theta_deg columns

--- erna/hdf_utils.py
+++ erna/hdf_utils.py
@@ -4,10 +4,31 @@ from astropy.io import fits
 from tqdm import tqdm
 import sys
 from fact.io import append_to_h5py, initialize_h5py
+from fact.instrument import camera_distance_mm_to_deg
+import re
+from numpy.lib import recfunctions
 
 log = logging.getLogger(__name__)
 
 native_byteorder = {'little': '<', 'big': '>'}[sys.byteorder]
+
+
+theta_columns = tuple(
+    ['theta'] + ['theta_off_{}'.format(i) for i in range(1, 6)]
+)
+
+theta_deg_columns = tuple(
+    ['theta_deg'] + ['theta_deg_off_{}'.format(i) for i in range(1, 6)]
+)
+
+snake_re_1 = re.compile('(.)([A-Z][a-z]+)')
+snake_re_2 = re.compile('([a-z0-9])([A-Z])')
+
+
+def camel2snake(key):
+    ''' see http://stackoverflow.com/a/1176023/3838691 '''
+    s1 = snake_re_1.sub(r'\1_\2', key)
+    return snake_re_2.sub(r'\1_\2', s1).lower().replace('__', '_')
 
 
 def write_fits_to_hdf5(
@@ -28,13 +49,27 @@ def write_fits_to_hdf5(
                 if len(f) < 2:
                     continue
 
+                array = f[1].data[:]
+
+                # convert all names to snake case
+                array.dtype.names = list(map(camel2snake, array.dtype.names))
+
+                # add columns with theta in degrees
+                for in_col, out_col in zip(theta_columns, theta_deg_columns):
+                    if in_col in array.dtype.names:
+                        recfunctions.append_fields(
+                            array,
+                            out_col,
+                            camera_distance_mm_to_deg(in_col)
+                        )
+
                 if not initialized:
                     initialize_h5py(
                         hdf_file,
-                        f[1].data.dtype,
+                        array.data.dtype,
                         key=key,
                         compression=compression,
                     )
                     initialized = True
 
-                append_to_h5py(hdf_file, f[1].data, key=key)
+                append_to_h5py(hdf_file, array, key=key)
