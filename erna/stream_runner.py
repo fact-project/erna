13086b733be4e00e1b617def3ed690b9ea13eb75
Jens Buss <jens.buss@udo.edu>
2017-05-22 12:20:03 +0200

    delete code that was commented out

--- erna/stream_runner.py
+++ erna/stream_runner.py
@@ -18,23 +18,6 @@ def run(jar, xml, df, num, db_path=None):
     '''
     logger = logging.getLogger(__name__)
     logger.info("stream runner has been started.")
-    # if 'DEFAULT_TEMP_DIR' not in os.environ:
-    #     logger.error("No scratch directory given via environment variable DEFAULT_TEMP_DIR. Aborting")
-    #     return "No default temp dir"
-    #
-    # output_directory = os.environ['DEFAULT_TEMP_DIR']
-    #
-    # if not os.path.isdir(output_directory):
-    #     logger.warn("Output directory {} does not exist. Trying to create it.".format(output_directory))
-    #     try:
-    #         os.mkdir(output_directory, mode=0o755)
-    #     except OSError: # Python >2.5
-    #             pass
-    #     #check if that worked or not
-    #     if not os.access(output_directory, os.W_OK | os.X_OK) :
-    #         logger.error("Cannot write to directory given DEFAULT_TEMP_DIR {} ".format(output_directory))
-    #         return "Cannot write to dir"
-
 
     with tempfile.TemporaryDirectory() as output_directory:
         name, _ = os.path.splitext(os.path.basename(xml))
@@ -56,5 +39,5 @@ def run(jar, xml, df, num, db_path=None):
             else:
                 return "fact-tools error"
 
-        #try to read nans else return empty frame
+        # try to read nans else return empty frame
         return ft_json_to_df(output_path)
