7d9753f3eed3571ba3e2f3aa07f9db4354793b81
Michael Bulinski <michael.bulinski@tu-dortmund.de>
2017-11-07 13:36:25 +0100

    added the new datacheck conditions for moon state

--- erna/datacheck_conditions.py
+++ erna/datacheck_conditions.py
@@ -12,3 +12,40 @@ conditions['darknight'] = [
     'fCurrentsMedMean < 20',
     'fMoonZenithDistance > 100',
 ] + conditions['standard']
+
+
+conditions['no_moonlight'] = [
+    'fCurrentsMedMeanBeg < 8',
+    'fZenithDistanceMax < 30',
+    'fMoonZenithDistance > 100',
+    'fThresholdMinSet < 350',
+    'fEffectiveOn > 0.95',
+    'fTriggerRateMedian > 40',
+    'fTriggerRateMedian < 85',
+    'fThresholdMinSet < (14 * fCurrentsMedMeanBeg + 265)'
+]
+
+conditions['low_moonlight'] = [
+    'fTriggerRateMedian < 85',
+    'fZenithDistanceMax < 30',
+    'fThresholdMinSet < (14 * fCurrentsMedMeanBeg + 265)',
+    'fCurrentsMedMeanBeg > 8',
+    'fCurrentsMedMeanBeg <= 16',
+]
+
+
+conditions['moderate_moonlight'] = [
+    'fTriggerRateMedian < 85',
+    'fZenithDistanceMax < 30',
+    'fThresholdMinSet < (14 * fCurrentsMedMeanBeg + 265)',
+    'fCurrentsMedMeanBeg > 32',
+    'fCurrentsMedMeanBeg <= 48',
+]
+
+conditions=['strong_moonlight'] = [
+    'fTriggerRateMedian < 85',
+    'fZenithDistanceMax < 30',
+    'fThresholdMinSet < (14 * fCurrentsMedMeanBeg + 265)',
+    'fCurrentsMedMeanBeg > 64',
+    'fCurrentsMedMeanBeg <= 96',
+]
